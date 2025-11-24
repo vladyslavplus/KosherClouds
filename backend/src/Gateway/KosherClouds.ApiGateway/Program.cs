9c1f3733e5343cb5ff057fcac7fe79992e73f4b6
vladyslavplus <vladislavperevispa12345@gmail.com>
2025-11-24 12:56:12 +0200

    feat: implement YARP Api gateway

--- backend/src/Gateway/KosherClouds.ApiGateway/Program.cs
+++ backend/src/Gateway/KosherClouds.ApiGateway/Program.cs
@@ -1,15 +1,29 @@
+using KosherClouds.ServiceDefaults.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddJwtAuthentication(builder.Configuration);
+
+builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+app.UseCors("AllowAll");
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapHealthChecks("/health");
+app.MapReverseProxy();
 await app.RunAsync();
